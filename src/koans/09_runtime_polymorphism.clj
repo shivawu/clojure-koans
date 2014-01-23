4f2f4d45edd3d11729c039ded4ce81d8036a14f0
Shiva Wu <shivaw@gmail.com>
2014-01-23 23:05:04 +0800

    solutions, 2014-01-23

--- src/koans/09_runtime_polymorphism.clj
+++ src/koans/09_runtime_polymorphism.clj
@@ -7,19 +7,19 @@
                    "!")))
 
 (defmulti diet (fn [x] (:eater x)))
-(defmethod diet :herbivore [a] __)
-(defmethod diet :carnivore [a] __)
-(defmethod diet :default [a] __)
+(defmethod diet :herbivore [a] "Bambi eats veggies.")
+(defmethod diet :carnivore [a] "Simba eats animals.")
+(defmethod diet :default [a] "I don't know what Rich Hickey eats.")
 
 (meditations
   "Some functions can be used in different ways - with no arguments"
-  (= __ (hello))
+  (= "Hello World!" (hello))
 
   "With one argument"
-  (= __ (hello "world"))
+  (= "Hello, you silly world." (hello "world"))
 
   "Or with many arguments"
-  (= __
+  (= "Hello to this group: Peter, Paul, Mary!"
      (hello "Peter" "Paul" "Mary"))
 
   "Multimethods allow more complex dispatching"
@@ -27,7 +27,7 @@
      (diet {:species "deer" :name "Bambi" :age 1 :eater :herbivore}))
 
   "Animals have different names"
-  (= "Thumper eats veggies."
+  (= "Bambi eats veggies."
     (diet {:species "rabbit" :name "Thumper" :age 1 :eater :herbivore}))
 
   "Different methods are used depending on the dispatch function result"
